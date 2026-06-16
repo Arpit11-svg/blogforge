0bea242fa168ccfdf098515051391a1169519e8f
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2026-06-16 23:06:40 +0530

    Add header and footer

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Button({
+  children, //give any name like text (e.g, Home, add todo etc)
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
