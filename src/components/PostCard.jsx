28fbdf61e99563bfc4028e2b9270c9142de1bc8c
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2026-06-17 15:44:06 +0530

    Add authentication UI components

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import appwriteService from "../appwrite/config"
 
 function PostCard({$id, title, featuredImage}) {
+  // Note: id came from appwrite with $id.
   return (
     <Link to={`/post/${$id}`}>
       <div className='w-full bg-gray-100 rounded-xl p-4'>
